61bde049c7d413c6386edd7a2b05d7f0eb34ec41
Isaac Wilson <113647541+iswilsons@users.noreply.github.com>
2023-02-15 15:00:04 +0000

    Fixed typo in documentation

--- get_help.c
+++ get_help.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_help - function that retrieves help messages according builtin
+ * get_help - function that retrieves help messages according to builtin
  * @datash: data structure (args and input)
  * Return: Return 0 (success)
 */
