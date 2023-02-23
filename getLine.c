a05d22ec1602153039e0cf97d687c97124e72c1c
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-02-22 21:31:22 -0500

    Updated the get_line.c

--- getLine.c
+++ getLine.c
@@ -5,7 +5,6 @@
  * @info: parameter struct
  * @buf: address of buffer
  * @len: address of len var
- *
  * Return: bytes read
  */
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
@@ -47,7 +46,6 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 /**
  * get_input - gets a line minus the newline
  * @info: parameter struct
- *
  * Return: bytes read
  */
 ssize_t get_input(info_t *info)
@@ -152,7 +150,6 @@ int _getline(info_t *info, char **ptr, size_t *length)
 /**
  * sigintHandler - blocks ctrl-C
  * @sig_num: the signal number
- *
  * Return: void
  */
 void sigintHandler(__attribute__((unused))int sig_num)
